6960453afa29259e8043bdcaa3b884c5a6938ff4
Vinícius Santiago <viniciussantiagosantos19@gmail.com>
2021-03-31 22:48:13 -0300

    conversor em dollar

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,14 +1,5 @@
 let valorEmDolarTexto = prompt("Qual o valor em dólar que você quer converter?");
-console.log(typeof valorEmDolarTexto);
-//string = texto
-
 let valorEmDolarNumero = parseFloat(valorEmDolarTexto);
-console.log(typeof valorEmDolarNumero);
-
-alert(valorEmDolarNumero);
-
 let valorEmReal = valorEmDolarNumero * 5.77;
-
 let valorEmRealFixado = valorEmReal.toFixed(2);
-
 alert(valorEmRealFixado);
